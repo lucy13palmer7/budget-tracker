dca50f3b566b7808517e2dc96e1d3ed151841fdd
Lucy <lucy13palmer7@gmail.com>
2020-03-11 19:40:54 -0600

    commit changes to server.js for MONGODB

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 3000;
+const PORT =  process.env.PORT || 3000;
 
 const app = express();
 
@@ -15,11 +15,17 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
-
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadLines";
+if(MONGODB_URI){
+mongoose.connect(MONGODB_URI);
+} 
+else 
+{
+  mongoose.connect("mongodb://localhost/budget", {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  });
+}
 // routes here
 
 app.listen(PORT, () => {
